6dd8dcb4ba92f79544ce598992ba38e16c7eff61
Arjun Kaushal <arjunkaushal01@gmail.com>
2024-09-09 19:52:23 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import mongoose from 'mongoose';
+
+console.log('MongoDB URI:', process.env.MONGO); // Should print the URI
+
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
+
+
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-    
-})
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
